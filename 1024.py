5ddd0c15e4242cf4e811bd472b37fe1eb44f43bd
MZRidaz <123804827+MZRidaz@users.noreply.github.com>
2023-05-16 13:34:10 +0800

    Update 1024.py

--- 1024.py
+++ 1024.py
@@ -7,27 +7,25 @@ import os
 from getver import GetVerificationCode
 from multiprocessing import Pool
 from config import config
-import pickle
-import json
 
 class Autoreply:
     result=None
     over=False
     flag=False
     UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
-    loginurl = 'https://t66y.com/login.php'
-    url='https://t66y.com/thread0806.php?fid=7&search=today'
+    loginurl = 'http://t66y.com/login.php'
+    url='http://t66y.com/thread0806.php?fid=7&search=today'
     headers={
         'Host': 't66y.com',
         'Proxy-Connection': 'keep-alive',
-        'Referer': 'https://t66y.com/index.php',
+        'Referer': 'http://t66y.com/index.php',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': UserAgent
     }
     headers1={
         'Host': 't66y.com',
         'Proxy-Connection': 'keep-alive',
-        'Referer': 'https://t66y.com/login.php',
+        'Referer': 'http://t66y.com/login.php',
         'User-Agent': UserAgent
     }
 
@@ -45,8 +43,8 @@ class Autoreply:
                 'pwpwd':  self.password,
                 'hideid': '0',
                 'cktime': '0',
-                'forward': 'https://t66y.com/post.php?',
-                'jumpurl': 'https://t66y.com/post.php?',
+                'forward': 'http://t66y.com/post.php?',
+                'jumpurl': 'http://t66y.com/post.php?',
                 'step': '2'
         }
         login=self.s.post(self.loginurl,headers=self.headers,data=data)
@@ -70,40 +68,20 @@ class Autoreply:
         'oneCode': str(my_token)
         }
         login=self.s.post(self.loginurl,headers=self.headers,data=data)
-        with open(f"./{self.user}", 'wb') as f:
-            pickle.dump(login.cookies, f)
         self.cookies=login.cookies
         login=login.content.decode('utf-8','ignore')
         if login.find('您已經順利登錄')!=-1:
             res='已經順利登錄'
             self.s.close()
             return res
-    
-    def updateCookies(self, cookie):
-        self.s.cookies.update(cookie)
-        self.cookies = self.s.cookies
-   
-    def verifyLoginSuc(self):
-        with self.s as s:
-            index = s.get('https://t66y.com/index.php', headers = {
-                'Host': 't66y.com',
-                'Upgrade-Insecure-Requests': '1',
-                'User-Agent': self.UserAgent
-            })
-            index = index.content.decode('utf-8','ignore')
-            if index.find('上次登錄時間')!=-1:
-                print('cookie有效登录')
-                return True
-            print('cookie无效登录, 切换为密码登录')
-            return False
-        
+
     def getcookies(self):
         return self.cookies
 
     def getverwebp(self):
         code=random.uniform(0,1)
         code=round(code,16)
-        vercodeurl='https://t66y.com/require/codeimg.php?'+str(code)
+        vercodeurl='http://t66y.com/require/codeimg.php?'+str(code)
         image=self.s.get(vercodeurl,headers=self.headers1)
         f=open('image.webp','wb')
         f.write(image.content)
@@ -165,7 +143,7 @@ class Autoreply:
     def getonelink(todaylist):
         geturl=''
         m=random.randint(0,len(todaylist)-1)
-        geturl='https://t66y.com/'+todaylist[m]
+        geturl='http://t66y.com/'+todaylist[m]
         tid=todaylist[m][16:len(todaylist[m])-5]
         todaylist.remove(todaylist[m])
         #print('请求链接是: '+geturl)
@@ -176,23 +154,18 @@ class Autoreply:
         headers={
         'Host': 't66y.com',
         'Proxy-Connection': 'keep-alive',
-        'Referer': 'https://t66y.com/index.php',
+        'Referer': 'http://t66y.com/index.php',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': Autoreply.UserAgent
         }
         res=requests.get(url=geturl,headers=headers,cookies=cookies)
-        reply_id = re.findall("<a name=#(\d+)><\/a>", res.text)
-        if len(reply_id)==0:
-            return None
-        else:
-            return reply_id[random.randint(0,len(reply_id)-1)]
 
     @staticmethod
     def getmatch(geturl,cookies):
         headers={
         'Host': 't66y.com',
         'Proxy-Connection': 'keep-alive',
-        'Referer': 'https://t66y.com/index.php',
+        'Referer': 'http://t66y.com/index.php',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': Autoreply.UserAgent
         }
@@ -209,7 +182,7 @@ class Autoreply:
     @staticmethod
     def getreply():
         #自定义回复内容，记得修改随机数
-        reply=['感谢分享','感谢你的分享','谢谢分享','多谢分享','感谢作者的分享','谢谢榴友分享','内容精彩','的确如此','感谢分享','涨知识了','很有意思']
+        reply=['感谢分享','感谢你的分享','谢谢分享','多谢分享','感谢作者的分享','谢谢坛友分享','内容精彩','的确如此','感谢分享','涨知识了','很有意思']
         reply_m=random.randint(0,len(reply)-1)
         reply_news=reply[reply_m]
         print('本次回复消息是:'+reply_news)
@@ -225,7 +198,7 @@ class Autoreply:
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': Autoreply.UserAgent
         }
-        posturl='https://t66y.com/post.php?'
+        posturl='http://t66y.com/post.php?'
         data={
             'atc_usesign':'1',
             'atc_convert':'1',
@@ -253,11 +226,11 @@ class Autoreply:
 
     @staticmethod
     def getnumber(cookies):
-        indexurl='https://t66y.com/index.php'
+        indexurl='http://t66y.com/index.php'
         headers={
         'Host': 't66y.com',
         'Proxy-Connection': 'keep-alive',
-        'Referer': 'https://t66y.com/index.php',
+        'Referer': 'http://t66y.com/index.php',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': Autoreply.UserAgent
         }
@@ -268,34 +241,6 @@ class Autoreply:
         num=re.search(pat,index).group(0)
         num=num.replace('共發表帖子: ','')
         return num
-    
-    @staticmethod
-    def like(id,url,cookies):
-        likeurl='https://t66y.com/api.php'
-        headers={
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Host': 't66y.com',
-        'Connection': 'keep-alive',
-        'Origin': 'https://t66y.com',
-        'User-Agent': Autoreply.UserAgent,
-        'X-Requested-With': 'XMLhttpRequest',
-        }
-        data={
-            'action': 'clickLike',
-            'id': id,
-            'page': 'h',
-            'json': '1',
-            'url': url
-        }    
-        like=requests.post(likeurl,headers=headers,data=data,cookies=cookies)
-        try:
-            if int(json.loads(like.text)['myMoney']) > 0 :
-                #print("点赞成功")
-                return True
-            return False
-        except:
-            return False
-
 
     @staticmethod
     def main(cookieslist,todaylist,ge):
@@ -303,7 +248,6 @@ class Autoreply:
         n=0
         cookies=cookieslist[ge]
         m=Autoreply.getnumber(cookies)
-        like_flag = False
         suc=False
         print('第'+str(ge+1)+'个账号开始时发表帖子:'+m)
         while n<10 and suc is False:
@@ -319,11 +263,7 @@ class Autoreply:
                     print('第'+str(ge+1)+'个账号回复成功')
                     n=n+1
                     print('第'+str(ge+1)+'个账号休眠'+str(sleeptime)+'s...')
-                    id = Autoreply.browse(geturl,cookies)
-                    if config.get('like',True) and id != False:
-                        temp = Autoreply.like(id,geturl,cookies)
-                        if like_flag != True:
-                            like_flag = temp
+                    Autoreply.browse(geturl,cookies)
                     sleep(sleeptime)
                     print('第'+str(ge+1)+'个账号休眠完成')
                 elif au=='今日已达上限':
@@ -340,8 +280,6 @@ class Autoreply:
         n=Autoreply.getnumber(cookies)
         print('第'+str(ge+1)+'个账号开始时发表帖子:'+m)
         print('第'+str(ge+1)+'个账号结束时发表帖子:'+n)
-        if config.get('like',True):
-            print(f"第{str(ge+1)}个账号点赞状态为{like_flag}")
         print('第'+str(ge+1)+'个账号回复'+str(int(n)-int(m))+'次')
 
 if __name__ == "__main__":
@@ -368,13 +306,6 @@ if __name__ == "__main__":
         success=None
         auto=Autoreply(userlist[count],passwordlist[count],secretlist[count])
         while success is None:
-            userCookieFile = f"./{userlist[count]}"
-            if os.path.isfile(userCookieFile):
-                with open(userCookieFile, 'rb') as f:   
-                    auto.updateCookies(pickle.load(f))
-                    print(f"账号{count} cookie文件已加载...跳过密码登录")
-                if auto.verifyLoginSuc() == True:
-                    break
             au=auto.login1()
             if au=='登录尝试次数过多,需输入验证码':
                 print('登录尝试次数过多,需输入验证码')
