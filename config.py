423fdc98f616bfdd27187226512eb8c9a4ac5989
MZRidaz <chenyin653@gmail.com>
2025-09-07 06:14:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-config = {
-    'Forbid': True,          # 是否禁用帖子过滤功能
-    'Input_self': False,     # 是否手动输入验证码
-    'like': True,            # 是否启用点赞功能
+config={
+    'Forbid':True,
+    'Input_self':False,
+    'like':True,
 }
