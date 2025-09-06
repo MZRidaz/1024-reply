9d6c4b701217e5139e02e908de21baf433cbf793
MZRidaz <chenyin653@gmail.com>
2025-09-07 06:08:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-config={
-    'Forbid':True,      # 是否移除版主帖子
-    'Input_self':False, # 是否手动输入验证码
-    'like':True,        # 是否点赞
+config = {
+    'Forbid': True,          # 是否禁用帖子过滤功能
+    'Input_self': False,     # 是否手动输入验证码
+    'like': True,            # 是否启用点赞功能
 }
