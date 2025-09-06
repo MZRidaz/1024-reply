e3d0ad4c2cebdfa0fd118e32709c7dbd2483d2de
MZRidaz <chenyin653@gmail.com>
2025-09-07 06:14:30 +0800

    Update getver.py

--- getver.py
+++ getver.py
@@ -7,30 +7,41 @@ import json
 class GetVerificationCode:
     @staticmethod
     def apitruecaptcha():
-        # TrueCaptcha 验证码服务
-        im = Image.open("image.webp").convert("RGB")
+        im=Image.open("image.webp")
         im.save('image.png')
-        with open('image.png', 'rb') as f:
-            userid = os.getenv("USERID")
-            apikey = os.getenv("APIKEY")
-            image = base64.b64encode(f.read())
-        url = 'https://api.apitruecaptcha.org/one/gettext'
-        data = {'data': str(image, 'utf-8'), 'userid': userid, 'apikey': apikey}
-        response = requests.post(url, json=data).json()
-        return response['result']
+        f=open('image.png','rb')
+        
+        userid = os.environ["USERID"]
+        apikey = os.environ["APIKEY"]
+        image=base64.b64encode(f.read())
+        url='https://api.apitruecaptcha.org/one/gettext'
+        data={
+            'data':str(image,'utf-8'),
+            'userid':userid,
+            'apikey':apikey
+        }
+        result = requests.post(url, json.dumps(data))
+        res=result.json()
+        code = res['result']
+        return code
 
     @staticmethod
     def ttshitu():
-        # TTSHITU 验证码服务
-        im = Image.open('image.webp').convert("RGB")
+        im=Image.open('image.webp')
         im.save('image.png')
-        with open('image.png', 'rb') as f:
-            image = base64.b64encode(f.read())
-        url = 'http://api.ttshitu.com/base64'
-        data = {
-            'username': os.getenv("CODEUSER"),
-            'password': os.getenv("CODEPASS"),
-            'image': image.decode('utf-8')
+        f=open('image.png','rb')
+        image=base64.b64encode(f.read())
+        host='http://api.ttshitu.com/base64'
+        headers={
+            'Content-Type':'application/json;charset=UTF-8'
         }
-        response = requests.post(url, json=data).json()
-        return response['data']['result']
+        data={
+            'username': os.environ["CODEUSER"] ,
+            'password': os.environ["CODEPASS"] ,
+            'image':image.decode('utf-8')
+        }
+        res=requests.post(url=host,data=json.dumps(data))
+        res=res.text
+        res=json.loads(res)
+        res=res['data']['result']
+        return res
